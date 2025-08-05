d76a79af6cf51d2d066597c757055f31014d33a3
jayss2717 <karand2717@gmail.com>
2025-08-05 22:46:34 +1000

    Remove X-Frame-Options from next.config.js to let middleware handle it completely

--- next.config.js
+++ next.config.js
@@ -58,7 +58,6 @@ const nextConfig = {
       {
         source: '/(.*)',
         headers: [
-          { key: 'X-Frame-Options', value: 'ALLOWALL' },
           { 
             key: 'Content-Security-Policy', 
             value: [
